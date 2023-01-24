235590af90292910e61ade91189095016d4001a6
Vladoxx <wladimirkochmansky@gmail.com>
2023-01-24 13:28:06 -0300

    Exercicio 01 - Primeiros passos

---
+++ modulo-01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,17 @@
+const myName = "Wlad"
+const birthCity = "Maracaibo"
+
+let birthYear = 1986
+
+
+ console.log(myName);
+ console.log(birthCity);
+//  console.log(birthYear);
+ 
+ birthYear = 2030
+ console.log(birthYear);
+
+ birthCity = "Rio de Janeiro"
+ console.log(birthCity);
+
+ // Está dando erro porque a variavél birthCity é uma constante é ela é imutável.
