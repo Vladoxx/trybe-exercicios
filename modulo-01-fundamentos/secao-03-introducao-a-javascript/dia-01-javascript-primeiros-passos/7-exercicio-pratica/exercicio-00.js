b18068a80f8a1ad5e15756f94cb7b97f0673b011
Vladoxx <wladimirkochmansky@gmail.com>
2023-01-25 12:48:23 -0300

    Exercicio 00 - Prática - Operações Matemáticas

---
+++ modulo-01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/7-exercicio-pratica/exercicio-00.js
@@ -0,0 +1,22 @@
+const a = 23;
+const b = 2;
+
+// Adição
+const adicao = a + b
+console.log(adicao);
+
+// Subtração
+const subtracao = a - b
+console.log(subtracao);
+
+// Multiplicação
+const multiplicacao = a * b
+console.log(multiplicacao);
+
+// Divisão
+const divisao = a / b
+console.log(divisao);
+
+// Módulo
+const modulo = a % b
+console.log(modulo);
