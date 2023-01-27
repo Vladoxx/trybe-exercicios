db95d5da84bf95a6ea8aa520f5e2e167ff610060
Vladoxx <wladimirkochmansky@gmail.com>
2023-01-26 21:28:31 -0300

    Exercicio 08 - Usando for criar um array de 1 até 25 e imprimir ele

---
+++ modulo-01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/1-exercicio-pratica/exercicio-08.js
@@ -0,0 +1,10 @@
+// Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+const arrNumeros = []
+
+for (let i = 1; i <= 25; i++) {
+    
+    arrNumeros.push(i)
+}
+
+console.log(arrNumeros);
